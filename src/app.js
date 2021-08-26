4b6a42e68e823e501473a96d222c1724ec5bb34b
alaref-codes <alarefabdo1@gmail.com>
2021-08-26 08:56:36 +0200

    The entire Blog project

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const morgran = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('../routes/blogRoutes');
+// Connecting to mongo db
+
+
+const dbURI =  'mongodb+srv://Alaref:uUYZcvwrlmYFGXJK@cluster0.n9u2f.mongodb.net/database?retryWrites=true&w=majority'
+mongoose.connect(dbURI , { useNewUrlParser: true ,  useUnifiedTopology: true } )
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err))
+
+app.set('view engine' , 'ejs');
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(morgran('dev'))
+
+app.use((req,res , next) => {
+    console.log('---------- NEW REQUEST MADE ----------');
+    next();
+});
+
+app.get('/' , (req,res) => {
+    res.redirect('/blogs')
+});
+
+app.get('/about' , (req,res) => {
+    res.render('about');
+});
+
+// Blog pages
+app.use('/.netlify/functions/app' ,blogRoutes)
+
+
+app.use( (req, res) => {
+    res.status(404).render('404');
+});
+
